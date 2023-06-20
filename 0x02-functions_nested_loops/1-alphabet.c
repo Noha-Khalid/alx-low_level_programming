583adb46c90cdd70e39bc662787bc0b51b8b65eb
Noha-Khalid <nohakh28998@gmail.com>
2023-06-20 06:58:50 -0700

    repo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a message to the console.
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	print_alphabet();
+	return (0);
+}
