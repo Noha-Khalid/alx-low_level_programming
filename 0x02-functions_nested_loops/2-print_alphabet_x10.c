99256d7a52c697fdac090a0d97aa9ed77d7d7cde
Noha-Khalid <nohakh28998@gmail.com>
2023-06-20 15:05:43 -0700

    repo

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * Description: This is c program .
+ */
+void print_alphabet_x10(void);
+{
+      int i , ch ;
+      
+      for (i=0 ; i <= 9 ; i++)
+     {
+         for (ch ='a' ; ch <= 'z' ; ch++)
+   
+      _putchar(ch);
+      _putchar('/n') ;
+     }
+}
