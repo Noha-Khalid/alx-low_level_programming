e6b2523f54db44261d67eaafb579449502088c19
Noha-Khalid <nohakh28998@gmail.com>
2023-06-20 06:22:01 -0700

    repo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a message to the console.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("/_putchar");
+	return (0);
+}
