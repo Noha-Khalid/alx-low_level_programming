861a6cc8ed2ce7599a51503b393125bda6523f29
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-23 17:01:16 +0300

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+* c program
+* Return: 0
+*/
+
+void print_most_numbers(void)
+{
+   int num ;
+   
+   for (num = 0 ;num <= 9 ; num ++)
+   {
+       if (num == 2 || num == 4)
+       _putchar (num +48) ;
+   }
+   _putcha('\n');
+}
