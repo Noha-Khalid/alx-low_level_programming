18d156eca9c69113ceeee4952f7b0c96c83fdac9
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-23 17:14:23 +0300

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include <main.h>
+/**
+* c program
+* Return: 0
+*/
+
+void print_diagonal(int n);
+{
+   int i , z   ;
+  
+           if (n <= 0)
+        _putchar('\n');
+        else 
+        {
+             for (i = 1 ; i <= n ; i++)
+             {
+                 for (z = 1 ; z <= i ; z++)
+             _putchar(' ');
+             _putchar(92) ;
+             _putchar('\n');
+        }
+     
+  
+}
