1cb6a59ed028959fc3cd4ac2314f671c2c1c22e4
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-23 17:05:48 +0300

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <main.h>
+/**
+* c program
+* Return: 0
+*/
+
+void more_numbers(void);
+{
+   int num , r , i ;
+   for (r = 1; r <= 10; r++ )
+   {
+       for (i =0 ; i <= 14; i++ )
+       {
+           num = i ;
+           if (i > 9)
+           {
+               _putchar(1+ 48);
+               num = i % 10;
+           }
+           _putchar(num + 48);
+           }
+       }
+       _putchar('\n');
+   }
+}
