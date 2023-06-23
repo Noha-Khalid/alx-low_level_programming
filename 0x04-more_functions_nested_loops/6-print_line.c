0fed5c0f57757803466bf71579e428c09bb8f185
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-23 17:09:26 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,21 @@
+#include <main.h>
+/**
+* c program
+* Return: 0
+*/
 
+void print_line(int n)
+{
+   int num  ;
+  
+           if (n <= 0)
+        _putchar('\n');
+        else 
+        {
+             for (num = 1 ; num <= n ; n++)
+             _putchar('_');
+             _putchar('\n');
+        }
+     
+  
+}
