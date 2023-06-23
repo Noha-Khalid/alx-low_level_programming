c88dd13507dfe475857fdd503d5791642ee2b3e3
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-23 16:58:06 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+* c program
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+   int num = 0 ;
+   
+   do {
+        _putchar(num + 48);
+        num++
+   } while (nim >= 0 && num <= 9)
+   _putchar('\n');
+}
