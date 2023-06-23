54be7517ef807bcc3193edce2bec8d255af71247
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-23 19:17:22 +0300

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <main.h>
+/**
+* c program
+* Return: 0
+*/
+
+int main(void)
+{
+int n;
+
+ for (n =1 ; n <= 100; n++)
+ {
+     if (n % 3 === && !n % 5 == 0)
+      printf("fizz");
+      else if (n % 3 === && n % 5 == 0)
+      printf("fizzBuzz");
+      else if (!n % 3 === && n % 5 == 0)
+      printf("Buzz");
+      else 
+        printf("%d" , n);
+        
+        if(num != 100)
+        printf(" ");
+        else 
+          printf("\n");
+ }
+ return(0);
+}
