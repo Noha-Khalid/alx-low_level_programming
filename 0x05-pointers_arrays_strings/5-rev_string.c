cfefbff87352164959891a87e6032ccaa19a12c7
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-26 17:51:13 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,11 +13,12 @@ char temp;
 
     /* Find the length of the string */
 for (len = 0; s[len] != '\0'; ++len)
-    /* Swap characters from the beginning and end of the string */
+{
 for (i = 0; i < len / 2; i++)
 {
 temp = s[i];
 s[i] = s[len - 1 - i];
 s[len - 1 - i] = temp;
+}
 }
 }
