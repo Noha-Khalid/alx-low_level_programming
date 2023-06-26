4d744e5842c09fb59f36eb0fd4649cc6eef69f5b
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-26 17:45:28 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,20 +8,20 @@
  */
 void rev_string(char *s)
 {
-    int i, len;
-    char temp;
+int i, len;
+char temp;
 
     /* Find the length of the string */
-    for (len = 0; s[len] != '\0'; len++)
-    {
+for (len = 0; s[len] != '\0'; len++)
+{
         /* Empty loop body */
-    }
+}
 
     /* Swap characters from the beginning and end of the string */
-    for (i = 0; i < len / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[len - 1 - i];
-        s[len - 1 - i] = temp;
-    }
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[len - 1 - i];
+s[len - 1 - i] = temp;
+}
 }
