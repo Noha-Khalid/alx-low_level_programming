8663a7ada45f305874d25415cd6e96ff015fe6e2
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-26 17:21:34 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to stdout.
+ * @c: The character to be written.
+ *
+ * Return: On success, returns the character written.
+ * On error, returns -1 and sets errno.
+ */
+int _putchar(char c)
+{
+    /* TODO: Implement the function */
+    /* Placeholder code to avoid compilation error */
+    return (1);
+}
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: Pointer to the string to be printed.
+ */
+void _puts(char *str)
+{
+    /* TODO: Implement the function */
+    /* Placeholder code to avoid compilation error */
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+    /* TODO: Write your code here */
+
+    return (0);
+}
