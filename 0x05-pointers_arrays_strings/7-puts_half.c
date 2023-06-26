352356e784bc37ce9885783c60de7a00ca80f3fa
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-06-26 17:58:51 +0300

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string.
+ * @str: Pointer to the string.
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+ int i;
+
+ for (i = 0; str[i] != '\0'; i++)
+ {
+        /* Empty loop body */
+ }
+
+ for (i /= 2; str[i] != '\0'; i++)
+ {
+  _putchar(str[i]);
+ }
+
+  _putchar('\n');
+}
