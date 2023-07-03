9221e9558fbd9d9f952f432cff26c827ccbca8d9
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-07-03 18:02:36 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,22 @@
+#include "n^in.h"
 
+/**
+ * _ncpy - Copies a string.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: Maximum number of bytes to be copied.
+ *
+ * Return: Pointer to the destination string (dest).
+ */
+char *_ncpy(char *dest, char *src, unsigned int n)
+{
+unsigned int itr;
+
+for (itr = 0; itr < n && src[itr] != '\0'; itr++)
+dest[itr] = src[itr];
+
+for (; itr < n; itr++)
+dest[itr] = '\0';
+
+return (dest);
+}
