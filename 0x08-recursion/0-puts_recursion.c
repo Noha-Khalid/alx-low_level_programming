1b82951bd1edd0d86b880f153176cfbacd10a01a
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-07-05 16:38:14 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,12 @@
+#include "main.h"
 
+void _puts_recursion(char *s)
+{
+    printf("%s\n", s); // Print the string s
+}
+
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return 0;
+}
