4e5ce8065224bd5638f5830224fb712c2b2ddf7d
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-07-05 17:56:31 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _print_rev_recursion - Prints a string reverse recursively followed by a new line.
- *
- * @s: The string to be printed.
- *
- * Description:
- * This function recursively prints each character of the input string reverse, @s,
- * followed by a new line character. If the string is empty (reached the null
- * terminator), it prints only the new line character.
- *
- * Return: None.
- */
+* _print_rev_recursion - Prints a string reverse recursively followed by a new line.
+*
+* @s: The string to be printed.
+*
+* Description:
+* This function recursively prints each character of the input string reverse, @s,
+* followed by a new line character. If the string is empty (reached the null
+* terminator), it prints only the new line character.
+*
+* Return: None.
+*/
 void _print_rev_recursion(char *s)
 {
 if (*s > '\0')
