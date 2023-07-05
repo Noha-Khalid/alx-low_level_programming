9aef14a9bb50c008b84c5e27b893904c1efa5a53
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-07-05 17:48:30 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,7 +11,7 @@
  *
  * Return: None.
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
 {
@@ -20,6 +20,6 @@ _putchar('\n');
 else
 {
 _putchar(*s);
-_puts_recursion(s - 1);
+_print_rev_recursion(s - 1);
 }
 }
