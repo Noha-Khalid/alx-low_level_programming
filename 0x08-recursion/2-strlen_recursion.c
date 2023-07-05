b5eab4171777923d6a27d411fa748066e83f8209
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-07-05 18:20:07 +0300

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse recursively.
+ *
+ * @s: The string to be printed in reverse.
+ *
+ * Description:
+ *     This function prints the characters of the input string @s in reverse
+ *     order recursively. It starts by calling itself with the next character
+ *     in the string until it reaches the null terminator. Then, it prints
+ *     each character in reverse order.
+ *
+ * Return: None.
+ */
+int _strlen_recursion(char *s)
+int n;
+n = _strlen_recursion("Corbin Coleman");
+printf("%d\n", n);
+return (0);
