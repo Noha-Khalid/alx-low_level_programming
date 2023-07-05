ec70fb0fbb868972ebab8669f0031a652cba2a75
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-07-05 19:41:50 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -21,16 +21,16 @@ int _pow_recursion(int x, int y)
 {
 if (y < 0)
 {
-return(-1);
+return (-1);
 }
 else if (y == 1)
 {
-return(x);
+return (x);
 }
 else if (y == 0)
 {
-return(1);
+return (1);
 }
     
-return( x * _pow_recursion(x , y-1));
+return (x * _pow_recursion(x , y-1));
 }
