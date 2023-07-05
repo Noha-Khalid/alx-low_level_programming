88c836f6e68f79b26b5e8cdc9e05cb9c00d6b60d
Noha-Khalid <116386121+Noha-Khalid@users.noreply.github.com>
2023-07-05 18:39:08 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,12 @@
 int _strlen_recursion(char *s)
 {
     /* Base case: if the current character is the null terminator */
-    if (*s == '\0')
-    {
-        return 0;
-    }
-    else
-    {
+if (*s == '\0')
+{
+return 0;
+}
+else
+{
 return _strlen_recursion(s + 1) + 1;
 }
 }
